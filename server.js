8f60f458fa6fff0ae79d68a0073cef88d7c59cec
ridoy2112 <you@example.com>
2023-01-22 21:12:40 +0600

    Six commit

--- server.js
+++ server.js
@@ -14,8 +14,9 @@ const react = require("./model/reactPost.js")
 const basic = require("./model/basic.js")
 const PORT = process.env.PORT || 5000;
 var app = express()
-app.use(cors())
 app.use(express.json())
+app.use(cors())
+app.use(cors({origin:['http://localhost:3000', 'https://portfolio-front-end-delta.vercel.app']}));
  
 mongoose.connect('mongodb+srv://Shahariar2112:ridaybabu7635@cluster0.l8cbdhl.mongodb.net/Prortfolio?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log("connected successfully")).catch((err) => console.log("it has an error", err));
 
