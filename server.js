75721e7f8db1d1f103b6478603ad11b00586b0a7
ridoy2112 <you@example.com>
2023-02-18 21:43:57 +0600

    14 commit

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 var app = express()
 app.use(express.json())
 app.use(cors())
-app.use(cors({origin:['http://localhost:3000', 'https://portfolio-front-end-delta.vercel.app']}));
+app.use(cors({origin:['http://localhost:3000', 'https://portfolio-front-end-beta.vercel.app']}));
  
 mongoose.connect('mongodb+srv://Shahariar2112:ridaybabu7635@cluster0.l8cbdhl.mongodb.net/Prortfolio?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log("connected successfully")).catch((err) => console.log("it has an error", err));
 
